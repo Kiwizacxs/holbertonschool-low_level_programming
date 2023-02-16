b5765fec502dffc8c1b27df4de525e45eeb2145f
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-16 08:06:53 -0800

    tres

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  *  * main - Entry point
@@ -7,5 +7,6 @@ nclude <stdio.h>
  *     */
 int main(void)
 {
-	    return (0);
+		printf("Holberton School");
+			return (0);
 }
