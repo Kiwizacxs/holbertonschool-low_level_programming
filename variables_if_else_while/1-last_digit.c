7be480592eed330bbc073ae62316ab957ff3554d
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-17 13:16:08 -0800

    unoo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 			printf("Last digit of %d is %d and is 0", n, carne);
 		}
 		else
-	if (carne < 6; carne != 0)
+	for (carne < 6; carne != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, carne);
 	}
