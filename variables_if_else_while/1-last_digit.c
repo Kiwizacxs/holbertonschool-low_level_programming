52e525395b2ff56ab22f04f4f2c31880e99e5b65
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-17 12:30:38 -0800

    unoo

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* *
+* main - 1234
+* Return: 0 
+*/
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+				carne = n  % 10
+					if (carne > 5)
+					{
+						printf("the string and is greater than 5")
+				return (0); 
