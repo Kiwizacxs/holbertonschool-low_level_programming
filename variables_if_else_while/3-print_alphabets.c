b8197f1938e9242f7f14da03c071246312f79586
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-17 15:24:12 -0800

    tres

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,12 @@ int main(void)
 {
 	char UP = 'A';
 	char down = 'a';
+	{
 	for (down = 'a'; down <= 'z'; down++)
 		putchar (UP);
 	for (UP = 'A'; UP <= 'Z'; UP++)
 		putchar (down);
 	putchar ('\n');
+	}
 	return (0);
 }
