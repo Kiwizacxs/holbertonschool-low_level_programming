161bd8e2cde54e72788f16cd43a274b852e338ce
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-17 15:44:25 -0800

    cuatro

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+* main - hola
+* Return: 0
+*/
+
+int main(void)
+{
+	char a = 'a'
+	{
+		for (a = 'a'; a <= 'z'; a++) if (a != q && a != e)
+			putchar (a)
+	}
+	return (0)
+}
