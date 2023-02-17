7ebe885b22f3946ad1912994a3ab1780aa85dea5
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-17 15:07:44 -0800

    tres

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+* main - 123
+* Return: 0
+*/
+
+int main(void)
+{
+	char UP = 'A';
+	char down = 'a';
+{
+	for (UP = 'A'; UP <= 'Z'; UP++)
+		putchar (UP)
+}
+{
+	for (down = 'a'; down <= 'z'; down++)
+		putchar (down);
+
+	putchar ('\n');
+}
+	return (0)
+}
