be26b55ba7fdc6e19a3f6576743981de29cb7b9a
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-17 13:41:17 -0800

    unoo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,22 +11,23 @@ int main(void)
 {
 	int n;
 	int carne;
-	 srand(time(0));
+
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	carne = n  % 10;
+	carne = n % 10;
 	if (carne > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, carne);
+	printf("Last digit of %d is %d and is greater than 5\n", n, carne);
 	}
 	else
-		if (carne == 0)
-		{
-			printf("Last digit of %d is %d and is 0", n, carne);
-		}
-		else
-			if (carne < 6)
+	if (carne == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, carne);
+	printf("Last digit of %d is %d and is 0\n", n, carne);
+	}
+	else
+	if (carne < 6 && carne != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, carne);
 	}
 	return (0);
 }
