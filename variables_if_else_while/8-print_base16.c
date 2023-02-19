b0d47d7caf1f8585d59b255ac40f59683d87ef2b
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-19 09:21:26 -0800

    ocho

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - 123
+* Return: 0
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+	{
+		for (a = 0; a <= 9; a++)
+			putchar ((a) + '0');
+	}
+	{
+		for (b = 'a'; b <= 'f'; b++)
+			putchar (b);
+	}
+return (0);
+}
