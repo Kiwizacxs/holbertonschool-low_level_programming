24aa0a01da072c0525ac7f6f95ba6a0adff041d1
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-19 12:10:50 -0800

    nueve

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main -1234
+* Return: 0
+*/
+
+int main(void)
+{
+	int a = 0;
+	{
+		for (a = 0; a <= 9; a++);
+		putchar (',');
+		putchar ('\t');
+		putchar ((a) + '0');
+}
+{
+	putchar ('$');
+}
+return (0);
+}
---
+++ variables_if_else_while/prueba.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main -1234
+* Return: 0
+*/
+
+int main(void)
+{
+	int a = 0;
+	int b = 9;
+	{
+		while (a <= b)
+		{
+			putchar ((a) + '0');
+			a++;
+		if (a <= 9)
+			{
+		putchar (',');
+		putchar ('\t');
+			}
+		}
+		putchar ('$');
+	}
+return (0);
+}
