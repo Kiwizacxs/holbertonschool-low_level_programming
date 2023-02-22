4753d00b5b1b53a65414f3b3aeffe1aff2b25cd2
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-22 04:51:44 -0800

    cuatro

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "maina.h"
+
+/**
+* _isalpha - 1234
+* Return: always 0.
+*/
+
+int _isalpha(int c)
+{
+	int b;
+	if (c >= 'A' && b <= 'Z')
+	{
+		return (1);
+	}
+		if (b >= 'a' && b <= 'z')
+		{
+		return (1);
+		}
+		else 
+			return (0);
+}
+
