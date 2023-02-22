095cca227c694ce984f5da27108656331b050ad2
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-22 05:59:33 -0800

    cinco

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,4 +13,18 @@ int print_sign(int n)
 		return (1);
 		_putchar ('+');
 	}
+	else
+		if (n = 0)
+		{
+			return (0);
+				_putchar (0);
+		}
+		else
+			if (n < 0)
+			{
+				return (-1);
+				_putchar ('-');
+			}
+			else
+				return (0);
 }
