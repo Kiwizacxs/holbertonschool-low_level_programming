be3a719a20301ad1e328e43ccc47f1a9cc246cd3
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-24 11:07:29 -0800

    cero

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper
+ * @c - hola
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	int A = 'A';
+	int Z = 'Z';
+	{
+		if (c >= A && c <= Z)
+		{
+		return (1);
+		}
+		else
+		return (0);
+	}
+}
