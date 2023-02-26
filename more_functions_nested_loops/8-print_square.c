54f0360d3cc29a17bd1a49735366e7423bfb5a7d
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-26 15:19:20 -0800

    ocho

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_square - holaa
+* @size: hola
+* Return: 0.
+*/
+
+void print_square(int size)
+{
+	int i = 0;
+	int e = 0;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (e = 0; e < size; e++)
+			{
+				_putchar (35);
+			}
+			_putchar ('\n');
+		}
+	}
+}
