16f205bff08a4673fb524d0c43f5905082135a6e
Kiwizacxs <brian.delon2016@utlook.com>
2023-02-26 14:56:12 -0800

    siete

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,23 @@
 
 void print_diagonal(int n)
 {
-	int a = 1;
-	int b = 1;
+	int a = 0;
+	int b = 0;
 
-	if (n <= 0)
-	{ 
-		_putchar ('\n');
-	}
-	else if (n > 0)
+       	if (n > 0)
 	{
-		for (a = 1; a <=  n; a++)
+		for (a = 0; a < n; a++)
 		{
-			_putchar (92);
-			_putchar ('\n');
-			for (b = 1; b <= a; b++)
+			for (b = 0; b < a; b++)
 			{
 			_putchar (' ');
 			}
+			_putchar (92);
+			_putchar ('\n');
 		}
+	}
+	if (n <= 0)
+	{
+		_putchar ('\n');
 	}
 }	
