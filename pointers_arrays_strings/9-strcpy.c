1341ea9e54b2039e250a1491929d1f199f6a7ba6
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-02 07:50:10 -0800

    nueve

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcpy  - check the code
+ * @dest: men
+ * @src: woman
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int b = 0;
+	int r = 0;
+
+	while (src[r] != '\0' + 1)
+	{
+		dest[b] = src[r];
+		r++;
+		b++;
+	}
+	return (dest);
+}
+
