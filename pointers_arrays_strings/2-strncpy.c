ceaa33141ba938bb127d328ccf69c10eb5ed60d9
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-05 09:45:46 -0800

    dos

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,15 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int b = 0;
 
-	while (b !=  n)
+	while (b <  n && src[b] != '\0')
 	{
 		dest[b] = src[b];
-		if (src[b]  == '\0')
-		{
-			break;
-		}
 		b++;
 	}
+	for ( ; b < n; b++)
+		dest[b] = '\0';
 	return (dest);
 
 }
