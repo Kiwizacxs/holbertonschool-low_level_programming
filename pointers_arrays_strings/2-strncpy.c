fb58d7448dd5be7e0340b29da3f265901887f551
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-05 08:06:49 -0800

    dos

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - leol
+ * @n: 1024
+ * @dest: xd
+ * @src: moon
+ * Return: 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b = 0;
+
+	while (b !=  n)
+	{
+		dest[b] = src [b];
+		if (dest[b] == '\0')
+		{
+			break;
+		}
+		b++;
+	}
+	return (dest);
+
+}
