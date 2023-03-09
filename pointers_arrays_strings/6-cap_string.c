dfeceab34490bda5871315493f3a2d2c0186165d
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-09 13:20:13 -0800

    seis

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,24 +6,22 @@ char
 	int b = 0;
 	int r = strlen(str);
 
-		while (b < r)
+	while (b < r)
+	{
+		if (str[b] >= 97 && str[b] <= 122)
 		{
-				if (str[b] >= 97 && str[b] <= 122)
-				{
-					if (str[b - 1] < 65 && str[b - 1] > 57)
-					{
-						str[b] = str[b] - 32;
-					}
-					if (str[b - 1] < 48 && str[b - 1] > 45)
-						str[b] = str[b] - 32;
-					else if (str[b - 1] < 45)
-						str[b] = str[b] - 32;
-					else if (str[b - 1] > 90 && str[b - 1] < 97)
-						str[b] = str[b] - 32;
-					else if (str[b - 1] > 122)
-						str[b] = str[b] - 32;
-				}
-			b++;
+			if (str[b - 1] < 65 && str[b - 1] > 57)
+				str[b] = str[b] - 32;
+			else if (str[b - 1] < 48 && str[b - 1] > 45)
+				str[b] = str[b] - 32;
+			else if (str[b - 1] < 45)
+				str[b] = str[b] - 32;
+			else if (str[b - 1] > 90 && str[b - 1] < 97)
+				str[b] = str[b] - 32;
+			else if (str[b - 1] > 122)
+				str[b] = str[b] - 32;
 		}
+		b++;
+	}
 	return (str);
 }
