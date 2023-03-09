cf6b3308b9e44fe82100deaa06c084824e2ffbd6
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-09 13:02:49 -0800

    seis

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,16 +10,16 @@ char
 		{
 				if (str[b] >= 97 && str[b] <= 122)
 				{
-					if (str[b - 1] < 65 && str[b - 1] > 45)
+					if (str[b - 1] < 65 && str[b - 1] > 57)
 					{
 						str[b] = str[b] - 32;
 					}
+					if (str[b - 1] < 48 && str[b - 1] > 45)
+						str[b] = str[b] - 32;
 					else if (str[b - 1] < 45)
 						str[b] = str[b] - 32;
 					else if (str[b - 1] > 90 && str[b - 1] < 97)
-					{
 						str[b] = str[b] - 32;
-					}
 					else if (str[b - 1] > 122)
 						str[b] = str[b] - 32;
 				}
