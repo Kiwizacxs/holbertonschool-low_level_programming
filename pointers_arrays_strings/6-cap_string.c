d0b1deaec934cac4bb005c49bdc580e774055c71
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-09 12:50:01 -0800

    seis

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+char
+*cap_string(char *str)
+{
+	int b = 0;
+	int r = strlen(str);
+
+		while (b < r)
+		{
+				if (str[b] >= 97 && str[b] <= 122)
+				{
+					if (str[b - 1] < 65)
+					{
+						str[b] = str[b] - 32;
+					}
+					else if (str[b - 1] > 90 && str[b - 1] < 97)
+					{
+						str[b] = str[b] - 32;
+					}
+					else if (str[b - 1] > 122)
+						str[b] = str[b] - 32;
+				}
+			b++;
+		}
+	return (str);
+}
