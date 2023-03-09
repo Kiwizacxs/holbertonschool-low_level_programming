bc763dd921c7ab850bcbd4457708a60f15ae075e
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-09 08:57:34 -0800

    seis

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,6 +2,8 @@
 
 int is_prime_number(int n)
 {
+	if (n < 0)
+		n = n * -1;
 	if (n % 2 == 0)
 		return (0);
 	else if (n % 3 == 0)
@@ -9,8 +11,6 @@ int is_prime_number(int n)
 	else if (n % 5 == 0)
 		return (0);
 	else if (n == 1)
-		return (0);
-	else if (n == -1)
 		return (0);
 	else
 		return (1);
