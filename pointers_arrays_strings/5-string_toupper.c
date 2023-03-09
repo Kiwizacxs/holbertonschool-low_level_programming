943ebf5973c3692a9f251359ade9291c2783adf2
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-09 11:43:42 -0800

    cinco

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - hola
+ * Return: 0.
+ */
+
+char *string_toupper(char *str)
+{
+	int b = 0;
+	int r = strlen(str);
+
+	for (b = 0; b <= r; b++)
+		if (str[b] >= 'a' && str[b] <= 122)
+		{
+			str[b] = str[b] -32;
+		}
+	return (str);
+}
