1a6c8643c5bf01ac116761bb959fa9b53485f1d9
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-10 14:54:51 -0800

    siete

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,35 +1,25 @@
 #include "main.h"
 
 char 
-*leet(char *s)
+*leet(char *l)
 {
-	int i = strlen(s);
-	char b[]= "43071";
+	int num[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int lett[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b = 0;
 	int r = 0;
 
-		while (r < i)
+	while (l[r] != '\0')
+	{
+		while (lett[b] != '\0')
 		{
-			if (s[r] == 'a')
-				s[r] = b[0];
-			else if (s[r] == 'A')
-				s[r] = b[0];
-			else if (s[r] == 'e')
-				s[r] = b[1];
-			else if (s[r] == 'E')
-				s[r] = b[1];
-			else if (s[r] == 'o')
-				s[r] = b[2];
-			else if (s[r] == 'O')
-				s[r] = b[2];
-			else if (s[r] == 't')
-				s[r] = b[3];
-			else if (s[r] == 'T')
-				s[r] = b[3];
-			else if (s[r] == 'l')
-				s[r] = b[4];
-			else if (s[r] == 'L')
-				s[r] = b[4];
-			r++;
+			if (l[r] == lett[b])
+			{
+				l[r] = num[b];
+			}
+			b++;
 		}
-		return (s);
+		b = 0;
+		r++;
+	}
+	return (l);
 }
