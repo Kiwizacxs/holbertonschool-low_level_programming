bf5551cffaeccef7250c313198a769ca309f8489
Kiwizacxs <brian.delon2016@utlook.com>
2023-03-12 19:55:27 -0700

    tres

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int
+main(int argc, char *argv[])
+{
+	int b, r, i;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	b = atoi(argv[1]);
+	r = atoi(argv[2]);
+	i = b * r;
+	printf("%d\n", i);
+	return (0);
+}
