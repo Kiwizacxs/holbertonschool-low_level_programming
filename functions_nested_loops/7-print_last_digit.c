705fb075d4e7fe12e6ac3bb2ab36e373b2bd8b2e
Kiwizacxs <6336@holbertonstudents.com>
2023-06-19 10:55:16 -0700

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+#include <limits.h>
 
 int print_last_digit(int n)
 {
+	if (n = INI_MIN)
+	{
+		_putchar('8');
+		return (8);
+	}
 	if (n < 0)
 	{
 		n = n * -1;
---
+++ functions_nested_loops/ini.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+	    printf("El valor mínimo para un entero con signo es: %d\n", INT_MIN);
+		    return 0;
+}
+
