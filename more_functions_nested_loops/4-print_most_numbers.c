cc142f99dfd9b37df14420e1c8a7df54a1c3101c
Kiwizacxs <6336@holbertonstudents.com>
2023-06-20 12:49:22 -0700

    numbres-24:x

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+void
+print_numbers(void)
+{
+	int a = 0;
+
+	while (a != 10)
+	{
+		if (a == 2 || a == 4)
+			a++;
+		_putchar(a + '0');
+		a++;
+	}
+	_putchar('\n');
+}
