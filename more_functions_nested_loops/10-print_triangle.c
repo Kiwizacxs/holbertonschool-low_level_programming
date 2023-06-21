44fc470998666e87d21228bb88d3e2f272729fb5
Kiwizacxs <6336@holbertonstudents.com>
2023-06-20 19:49:12 -0700

    triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,21 +6,24 @@ print_triangle(int size)
 {
 	int b = 0, a = size;
 
-	while (b != size)
-	{
-		while (a != 1)
-		{
-			_putchar(' ');
-			a--;
-		}
-		a = size;
-		while (a - b != size + 1)
-		{
-			_putchar(35);
-			a++;
-		}
+	if (size <= 0)
 		_putchar('\n');
-		b++;
-		a = size - b;
-	}
+	else
+		while (b != size)
+		{
+			while (a != 1)
+			{
+				_putchar(' ');
+				a--;
+			}
+			a = size;
+			while (a - b != size + 1)
+			{
+				_putchar(35);
+				a++;
+			}
+			_putchar('\n');
+			b++;
+			a = size - b;
+		}
 }
