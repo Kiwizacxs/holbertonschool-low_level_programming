93a0ec938cc438319d1d0b7eeee4427bd27d02cf
Kiwizacxs <6336@holbertonstudents.com>
2023-06-25 19:20:34 -0700

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,14 +7,15 @@ _atoi(char *s)
 {
 	unsigned int num = 0;
 	int count = 0;
-	int sig = 0;
+	int sign_positive = 0;
+	int sign_negative = 0
 
 	while (s[count] != '\0')
 	{
 		if (s[count] == 45)
-			sig--;
+			sign_negative++;
 		else if (s[count] == 43)
-			sig++;
+			sig_positive++;
 		if (s[count] >= 48 && s[count] <= 57)
 		{
 			num = num + s[count] % 48;
@@ -25,7 +26,9 @@ _atoi(char *s)
 		}
 		count++;
 	}
-	if (sig < 0)
+	if (sign_negative = 1)
+		num = num * -1;
+	else if (sign_negative > sign_positive)
 		num = num * -1;
 	return (num);
 }
