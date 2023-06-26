6e877ea9abb61e848001b1d7efa2d290a1f27928
Kiwizacxs <6336@holbertonstudents.com>
2023-06-26 09:56:00 -0700

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+char
+*_strcat(char *dest, char *src)
+{
+	int a = strlen(dest), b = 0;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
