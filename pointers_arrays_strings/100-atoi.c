e5a26a1fb795ca779b414f708c8398e09df2d778
Kiwizacxs <6336@holbertonstudents.com>
2023-06-25 19:07:19 -0700

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+int
+_atoi(char *s)
+{
+	int num = 0;
+	int count = 0;
+	int sig = 0;
+
+	while (s[count] != '\0')
+	{
+		if (s[count] == 45)
+			sig--;
+		else if (s[count] == 43)
+			sig++;
+		if (s[count] >= 48 && s[count] <= 57)
+		{
+			num = num + s[count] % 48;
+			if (s[count + 1] >= 48 && s[count + 1] <= 57)
+				num = num * 10;
+			else
+				break;
+		}
+		count++;
+	}
+	if (sig < 0)
+		num = num * -1;
+	return (num);
+}
