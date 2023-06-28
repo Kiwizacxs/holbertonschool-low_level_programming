b4e9d1aad3da754465caba34be118c4c860f601d
Kiwizacxs <6336@holbertonstudents.com>
2023-06-27 18:03:24 -0700

    strncp

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 char
 *_strncpy(char *dest, char *src, int n)
 {
 	int r = 0;
+	int a = strlen(src);
 
+	if (a < n)
+		n = a + 1;
 	while (r != n)
 	{
 		dest[r] = src[r];
 		r++;
-		if (src[r] == '\0')
-		{
-			dest[r] = '\0';
-			break;
-		}
 	}
 
 	return (dest);
