8c74ae0049a075c4857190c2969d99a836c2954e
Kiwizacxs <6336@holbertonstudents.com>
2023-07-02 16:37:49 -0700

    root recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	else if (a * a > b)
+		return (-1);
+	else
+		return (check(a + 1, b));
+}
+
+int 
+_sqrt_recursion(int n)
+{
+	int root;
+
+	if (n < 0)
+		return (-1);
+	else
+		root = check(1, n);
+	return (root);
+}
+
