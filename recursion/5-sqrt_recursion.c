e68688255ef9a4de3ffbbf4ae66980a5f3fc2cea
Kiwizacxs <6336@holbertonstudents.com>
2023-07-02 16:39:29 -0700

    root recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,15 +10,14 @@ int check(int a, int b)
 		return (check(a + 1, b));
 }
 
-int 
+int
 _sqrt_recursion(int n)
 {
 	int root;
 
 	if (n < 0)
 		return (-1);
-	else
-		root = check(1, n);
+	root = check(1, n);
 	return (root);
 }
 
