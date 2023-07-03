1199d79b7cc6ee7e8e9d2963d99f339937f1926a
Kiwizacxs <6336@holbertonstudents.com>
2023-07-02 19:01:38 -0700

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int
 main(int argc, char *argv[])
 {
 	int a = 1;
 	int b = 0;
-	int c;
+	int c, d;
 
 	while (a != argc)
 	{
-		c = atoi(argv[a]);
+		for (d = 0; argv[a][d] != '\0'; d++)
+			if (argv[a][d] > 57 || argv[a][d] < 48)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		c = atol(argv[a]);
 		if (c == 0)
 		{
 			printf("Error\n");
