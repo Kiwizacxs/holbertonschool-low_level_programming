0e0749f5f83b2c4243319df71990d1fc7c8b3b00
Kiwizacxs <6336@holbertonstudents.com>
2023-07-08 14:20:02 -0700

    concat malloc

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char
+*str_concat(char *s1, char *s2)
+{
+	char *c;
+	int d = 0;
+	int a = 0;
+
+	c = malloc(strlen(s1) + strlen(s2) + 1);
+	if (c == NULL)
+	{
+		free(c);
+		return (NULL);
+	}
+	while (s1[d] != '\0')
+	{
+		c[d] = s1[d];
+		d++;
+	}
+	while (s2[a] != '\0')
+	{
+		c[d] = s2[a];
+		d++;
+		a++;
+	}
+	c[d] = '\0';
+	return (c);
+}
