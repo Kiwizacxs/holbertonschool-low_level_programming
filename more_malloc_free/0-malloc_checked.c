c97de69db617146c07732d1a6b2e1733d02352fe
Kiwizacxs <6336@holbertonstudents.com>
2023-07-09 16:37:36 -0700

    memory allocation

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+	
+void
+*malloc_checked(unsigned int b)
+{
+	char *c;
+
+	c = malloc(b);
+	if (c == NULL)
+	{
+		exit(98);
+		return (NULL);
+	}
+	return (c);
+}
+
