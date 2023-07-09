3504d69eeb08c394aa455b9f78544e95b1fec14c
Kiwizacxs <6336@holbertonstudents.com>
2023-07-09 06:00:32 -0700

    array malloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+int
+**alloc_grid(int width, int height)
+{
+	int **a;
+	int b;
+	int c = 0;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	a = (int **)malloc(height * sizeof(int *));
+	if (a == NULL)
+		return (NULL);
+	for (b = 0; b < height; b++)
+	{
+		a[b] = (int *)malloc(width * sizeof(int));
+		if (a[b] == NULL)
+			return (NULL);
+		for (c = 0; c < width; c++)
+		{
+			a[b][c] = 0;
+		}
+	}
+	return (a);
+}
+
