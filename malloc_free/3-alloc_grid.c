4e75fe3956288109e464c3b901445ee3973a97df
Kiwizacxs <6336@holbertonstudents.com>
2023-07-09 06:07:55 -0700

    malloc array

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,14 +14,18 @@ int
 	a = (int **)malloc(height * sizeof(int *));
 	if (a == NULL)
 	{
-		free (a);
+		free(a);
 		return (NULL);
 	}
 	for (b = 0; b < height; b++)
 	{
 		a[b] = (int *)malloc(width * sizeof(int));
 		if (a[b] == NULL)
+		{
+			for (c = 0; c < b; c++)
+				free(a[c]);
 			return (NULL);
+		}
 		for (c = 0; c < width; c++)
 		{
 			a[b][c] = 0;
