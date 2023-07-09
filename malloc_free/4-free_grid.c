fad3cb1fbc6d372e8d791b17b37cae5f876ca54f
Kiwizacxs <6336@holbertonstudents.com>
2023-07-09 16:10:09 -0700

    free array

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+void
+free_grid(int **grid, int height)
+{
+	int b = 0;
+
+	while (b != height)
+	{
+		free(grid[b]);
+		b++;
+	}
+}
+
