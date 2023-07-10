04c0004ba87f5eac16ed27d6c49eaef7aa37c8b8
Kiwizacxs <6336@holbertonstudents.com>
2023-07-09 17:22:26 -0700

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+void
+*_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *c;
+	unsigned int a = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	c = malloc(nmemb * size);
+		if (c == NULL)
+		{ 
+			return (NULL);
+		}
+	while (a != nmemb)
+	{
+		c[a] = 0;
+		a++;
+	}
+	return (c);
+}
+
