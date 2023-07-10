c510844647de8f3c359401c936e52bb9b94f3b09
Kiwizacxs <6336@holbertonstudents.com>
2023-07-09 18:28:23 -0700

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@ void
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	c = malloc(nmemb * size * 4);
+	c = malloc(nmemb * (size * sizeof(int)));
 		if (c == NULL)
 			return (NULL);
 	for (a = 0; c[a] != '\0'; a++)
