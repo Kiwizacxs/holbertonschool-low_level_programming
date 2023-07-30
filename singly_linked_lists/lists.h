5efae2ffdb1e8bbf5a5e128daaa32826ed3d9395
Kiwizacxs <6336@holbertonstudents.com>
2023-07-30 16:05:53 -0700

    single list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,18 +1,18 @@
 #ifndef HOLA
 #define HOLA
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
 /**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
- *        */
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
 typedef struct list_s
 {
 	char *str;
