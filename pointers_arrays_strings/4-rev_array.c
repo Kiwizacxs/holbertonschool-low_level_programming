c55e2052dfe2f1ce42f173989405eab3abe6a86f
Kiwizacxs <brian.deleon2016@outlook.com>
2023-08-29 23:04:02 +0000

    rev array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void reverse_array(int *a, int n)
+{
+    int len = 0;
+    int aux = 0;
+    int i = n / 2;
+
+    n--;
+    while (len != i)
+    {
+        aux = a[len];
+        a[len] = a[n];
+        a[n] = aux;
+        len++;
+        n--;
+    }
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#include <stddef.h>
+#include <stdio.h>
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
@@ -14,3 +17,4 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
