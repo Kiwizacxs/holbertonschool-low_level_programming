e66aedb84c55ca4073d878457a934a920be9b7c7
Kiwizacxs <brian.deleon2016@outlook.com>
2023-08-30 00:29:30 +0000

    change of a uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+char
+*string_toupper(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (str[len] > 90)
+		{
+			str[len] = str[len] - 32;
+		}
+		len++;
+	}
+	return(str);
+}
