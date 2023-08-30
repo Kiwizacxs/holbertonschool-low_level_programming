2a13658ab8e454529060ce68e3570eee5cd6e701
Kiwizacxs <brian.deleon2016@outlook.com>
2023-08-30 00:15:06 +0000

    rev array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -5,14 +5,14 @@ reverse_array(int *a, int n)
 {
 	int len = 0;
 	int aux = 0;
-	int i = n / 2;
+	int i;
 
 	if (n < 0)
 	{
-		n = 14;
+		n = 13;
 	}
+	i = n / 2;
 	n--;
-	len = 0;
 	while (len != i)
 	{
 		aux = a[len];
