ce09aabe458e485592ee80cdc1911ea5996fbb9b
Kiwizacxs <brian.deleon2016@outlook.com>
2023-09-17 13:08:24 +0000

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,12 +14,16 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t len = 0;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
 	while (len != size && array[len] != value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", len, array[len]);
 		len++;
 	}
-	printf("Value checked array[%ld] = [%d]\n", len, array[len]);
 	if (len == size)
 	{
 		return (-1);
