089dc1cbfad29cf31716ed750b5075d767ff937c
Kiwizacxs <brian.deleon2016@outook.com>
2023-09-18 00:02:45 +0000

    binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Function that searches for a value in an array of
+ * binary_search - Function that searches for a value in an array of
  * integers using the Linear search algorithm.
  * @array: is a pointer to the first element of the array to search in.
  * @size: is the number of elements in array.
@@ -25,40 +25,29 @@ int binary_search(int *array, size_t size, int value)
 		finish = 0;
 		printf("Searching in array: ");
 		while (len != size && array[len] != value)
-		{
 			len++;
-		}
 		while (i != size - 1)
 		{
 			printf("%d, ", array[i]);
-			i++;
-			finish++;
+			i++, finish++;
 		}
 		printf("%d\n", array[i]);
-
 		if(array[len] != value || len == size)
 		{
-			finish++;
-			fail = -1;
+			finish++, fail = -1;
 		}
-		
 		if (len >= size - aux / 2)
 		{
-			len = size - aux / 2;
-			i = size - aux / 2;
+			len = size - aux / 2, i = size - aux / 2;
 		}
 		else
 		{
-			size--;
-			size = size / 2;
-			i = 0;
+			size--, size = size / 2, i = 0;
 		}
 		aux = aux / 2;
 	}
 	if (fail == -1)
-		{
-			return (-1);
-		}
+		return (-1);
 
 	return (len);
 }
